2e5fdb885b59ae26490410419dbf68a0be45b9b0
Nguonchhay Touch <nguonchhay@web-essentials.co>
2019-11-06 12:58:09 +0700

    [IMP] Complete contact form UI

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import useForm from 'react-hook-form';
 
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const ContactForm = props => {
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = data => {
-        console.log(data)
+        console.log(data);
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <FormGroup>
-                <Label for="full_name">Full Name *</Label>
-                <Input type="text" name="full_name" id="full_name" placeholder="First Name Last Name" innerRef={register({required: true})} />
-                {errors.full_name && <span>This field is required</span>}
-            </FormGroup>
+        <div>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Row form>
+                    <Col sm={6}>
+                        <FormGroup>
+                            <Label for="last_name">First Name *</Label>
+                            <Input type="text" name="last_name" id="first_name" placeholder="First Name" innerRef={register({required: true})} />
+                            {errors.last_name && <span>This field is required</span>}
+                        </FormGroup>
+                    </Col>
+                    <Col sm={6}>
+                        <FormGroup>
+                            <Label for="last_name">Last Name *</Label>
+                            <Input type="text" name="last_name" id="last_name" placeholder="Last Name" innerRef={register({required: true})} />
+                            {errors.last_name && <span>This field is required</span>}
+                        </FormGroup>
+                    </Col>
+                </Row>
+                
 
-            <FormGroup>
-                <Label for="email">Email *</Label>
-                <Input type="email" name="email" id="email" placeholder="Email Address" innerRef={register({required: true})} />
-                {errors.email && <span>This field is required</span>}
-            </FormGroup>
+                <FormGroup>
+                    <Label for="email">Email *</Label>
+                    <Input type="email" name="email" id="email" placeholder="Email Address" innerRef={register({required: true})} />
+                    {errors.email && <span>This field is required</span>}
+                </FormGroup>
 
-            <FormGroup>
-                <Button>Submit</Button>
-            </FormGroup>
-        </Form>
+                <FormGroup>
+                    <Label for="subject">Subject *</Label>
+                    <Input type="text" name="subject" id="subject" placeholder="Subject" innerRef={register({required: true})} />
+                    {errors.subject && <span>This field is required</span>}
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="subject">Message *</Label>
+                    <Input type="textarea" name="message" id="message" placeholder="Message" innerRef={register({required: true})} />
+                    {errors.message && <span>This field is required</span>}
+                </FormGroup>
+
+                <FormGroup>
+                    <Button>Submit</Button>
+                </FormGroup>
+            </Form>
+        </div>
     )
 }
 
